aa813691991cc714f420300c8ecaf6b5f9875d18
dev-moshiur <dev.moshiurr@gmail.com>
2022-12-18 05:21:05 -0800

    committed

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 
 
 const router = require("express").Router();
-const User = require("../models/User");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
 const dotenv = require("dotenv");
